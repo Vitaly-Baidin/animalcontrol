cfe5daf142909c7ef797d6b3f1ac44b2d4674aed
Vitaly-Baidin <vitaliy.baidin.a@gmail.com>
2022-07-07 13:11:14 +0300

    add payload

---
+++ src/main/java/com/viskei/animalcontrol/payload/response/MessageResponse.java
@@ -0,0 +1,12 @@
+package com.viskei.animalcontrol.payload.response;
+
+import lombok.Data;
+
+@Data
+public class MessageResponse {
+	private String message;
+
+	public MessageResponse(String message) {
+	    this.message = message;
+	  }
+}
