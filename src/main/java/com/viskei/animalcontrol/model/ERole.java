00f9f9fd31e9365289c7572264c62ca3bfffa0a3
Vitaly-Baidin <vitaliy.baidin.a@gmail.com>
2022-07-07 12:52:47 +0300

    create models and auto insert to DB -> ROLE

---
+++ src/main/java/com/viskei/animalcontrol/model/ERole.java
@@ -0,0 +1,7 @@
+ package com.viskei.animalcontrol.model;
+
+public enum ERole {
+  ROLE_USER,
+  ROLE_MODERATOR,
+  ROLE_ADMIN
+}
---
+++ src/main/java/com/viskei/animalcontrol/model/Role.java
@@ -0,0 +1,29 @@
+package com.viskei.animalcontrol.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Role {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Enumerated(EnumType.STRING)
+  @Column(length = 20)
+  private ERole name;
+
+  public Role(ERole name) {
+    this.name = name;
+  }
+}
