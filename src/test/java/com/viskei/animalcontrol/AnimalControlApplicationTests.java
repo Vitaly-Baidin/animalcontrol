72cef04a0c670afb07ac1a1ed97e3ee04331b222
Vitaly-Baidin <vitaliy.baidin.a@gmail.com>
2022-07-07 12:32:26 +0300

    init

---
+++ src/test/java/com/viskei/animalcontrol/AnimalControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viskei.animalcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
